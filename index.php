843358ff9995d446744c01e937dcc56893caf3a0
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-05-01 10:00:31 +0800

    Finished the 475px size responsive on both header and home page content

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
   <link rel="stylesheet" href="styles/style.css">
   <link rel="stylesheet" href="styles/modernize.css">
   <link rel="stylesheet" href="styles/utility.css">
+  <link rel="stylesheet" href="styles/header.css">
+  <link rel="stylesheet" href="styles/main.css">
+  <link rel="stylesheet" href="styles/homePage.css">
   <!-- karla font -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -17,6 +20,20 @@
   <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&family=Karla:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 </head>
 <body>
-  
+  <header class="header__container container">
+    <img src="assets/images/logo.png" alt="" class="logo__png">
+  </header>
+  <main>
+    <section class="home__page container">
+      <div class="home__content">
+        <h1>WHAT IS YOUR ZODIAC SIGN?</h1>
+        <button>CONTINUE</button>
+      </div>
+    </section>
+    <section class="input__page container">
+
+    </section>
+  </main>
+  <footer></footer>
 </body>
 </html>
